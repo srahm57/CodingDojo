66e4d4fed1092ef787c0f6cb93d6f4edc3e8274f
Saifur <41875289+srahm57@users.noreply.github.com>
2018-08-28 12:21:17 +0530

    Update test.c

--- AdvanceAlgo/test.c
+++ AdvanceAlgo/test.c
@@ -1,14 +1,29 @@
-public static void findRotationPoint(int[] A) {
-	int start = 0, end = A.length - 1, mid;
+#include <stdio.h>
+
+int findPivot(int *arr) {
+	int start = 0, end = 4, mid;
+	int last = arr[end];
 	mid = start + (end - start) / 2;
-	int last = A[A.length - 1];
 	while (start <= end) {
-		if (A[mid] > last) {
+		if (arr[mid] > last) {
 			start = mid + 1;
-		} else if (A[mid] < last) {
+			}
+		else if (arr[mid] < last) {
 			end = mid - 1;
-		} else
+		} 
+		else{
 			break;
+		}
 		mid = start + (end - start) / 2;
 	}
-	
+	return(mid);
+}
+int main(){
+	int k, i, arr[5];
+	for(i=0; i<5; i++){
+		scanf("%d", &arr[i]);
+		}
+	k = findPivot(arr);
+	printf("Index of pivot: %d", k-1);
+	return 0;
+}
